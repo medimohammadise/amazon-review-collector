641a430c7302067f00a24c8d72cd9347088f5108
mehdi-mobilityone <mehdi@mobilityonegroup.com>
2021-12-15 00:27:45 +0100

    [FIX] Rating and review date has been provided by the scrapper

--- src/main/kotlin/com/example/stockservice/Review.kt
+++ src/main/kotlin/com/example/stockservice/Review.kt
@@ -2,5 +2,5 @@ package com.example.stockservice
 
 import java.time.LocalDate
 
-data class Review( val productId:String, val customerProfileId:String, val title:String , val author:String,val reviewText:String ,
+data class Review( val reviewId:String,  val rating:Int,val customerProfileId:String, val title:String , val author:String,val reviewText:String ,
                    val reviewDate:LocalDate)
--- src/main/kotlin/com/example/stockservice/service/AmazonReviewService.kt
+++ src/main/kotlin/com/example/stockservice/service/AmazonReviewService.kt
@@ -13,73 +13,76 @@ import java.util.regex.Pattern
 
 @Service
 class AmazonReviewService {
-    fun runReviewExtraction(url: String, productID: String): List<CompletableFuture<List<Review>>>{
-        var pageNumber= 1
+    fun runReviewExtraction(url: String, productID: String): List<CompletableFuture<List<Review>>> {
+        var pageNumber = 1
         var futures: MutableList<CompletableFuture<List<Review>>> = mutableListOf()
         do {
             val doc =
                 DocumentFactory.factory { CustomDocument(url = url, productId = productID, pageNumber = pageNumber) }
-            val task1= Supplier<Elements> {
+            val task1 = Supplier<Elements> {
                 doc?.reviewElements
 
             }
             val future: CompletableFuture<List<Review>> =
-                CompletableFuture.supplyAsync(task1).thenApply { doc-> extractLatestReviews(doc) }
+                CompletableFuture.supplyAsync(task1).thenApply { doc -> extractLatestReviews(doc) }
             futures.add(future)
             pageNumber++
-        }
-        while (doc?.pageNumber!=0)
+        } while (doc?.pageNumber != 0)
         return futures
     }
+
     fun extractLatestReviews(pageDocument: Elements?): List<Review> {
         var reviews: MutableList<Review> = arrayListOf();
         var allReviewsInPage: Elements
-       // var pageNo = 1
+        // var pageNo = 1
 
         val pattern: Pattern = Pattern.compile("(\\d+)")
-        if (pageDocument!=null) {
+        if (pageDocument != null) {
             /*pageSize = Integer.valueOf(document.get().select("div[id~=cm_cr]")
                 .select("li[class~=page-button]").last().
                             select("a").first().html());*/
-                allReviewsInPage = pageDocument.select("div[data-hook=review]")
-                allReviewsInPage.forEach { c ->
-                    val rating = ""
-                    /*val macther: Matcher = pattern.matcher(
-                        c.select("div[id^=customer_review]")
-                            .select("div[data-hook^=review-star-rating]").first().getElementsByClass("a-icon-alt").html()
+            allReviewsInPage = pageDocument.select("div[data-hook=review]")
+            allReviewsInPage.forEach { c ->
+                val ratingText = c.select("i[data-hook=review-star-rating] span[class=a-icon-alt]").html()
+                val ratingPattern =
+                    "[0-9].[0-9]".toRegex()
+                val rating= Integer.valueOf(ratingPattern.find(ratingText)?.value?.substringBefore("."))
+                val reviewBody: String =
+                    c.select("div[id^=customer_review] span[class=cr-original-review-content]").html()
+                val reviewTitle: String =
+                    c.select("a[data-hook=review-title] span[class=cr-original-review-content]").html()
+                val reviewAuthor: String =
+                    c.select("span[class=a-profile-name]").html()
+                var reviewDateText: String =
+                    c.select("span[data-hook=review-date]").html()
+                val pattern =
+                    "\\d{1,2}\\s(January|February|March|April|May|Jun|July|August|September|October|November|December)\\s\\d{4}".toRegex()
+                val match = pattern.find(reviewDateText)
+                val formatter1 = DateTimeFormatter.ofPattern("dd MMMM yyyy")
+                val formatter2 = DateTimeFormatter.ofPattern("d MMMM yyyy")
+                var localReviewDate=LocalDate.now()
+                try {
+                     localReviewDate = LocalDate.parse(match?.value, formatter1)
+                }
+                catch(ex:Exception)
+                {
+                     localReviewDate = LocalDate.parse(match?.value, formatter2)
+                }
+                // if (localReviewDate.isAfter(startDate)) { TODO develop this logic later
+                // println("new review received")
+                reviews.add(
+                    Review(
+                        reviewId=c.id(),
+                        rating=rating,
+                        customerProfileId = c.select("div[id^=customer_review]").attr("id")
+                            .substring("customer_review-".length),
+                        title = reviewTitle,
+                        author = reviewAuthor,
+                        reviewText = reviewBody,
+                        reviewDate = localReviewDate
                     )
-                    macther.find()*/
-                    val reviewBody: String =
-                        c.select("div[id^=customer_review] span[class=cr-original-review-content]").html()
-                    val reviewTitle: String =
-                        c.select("a[data-hook=review-title] span[class=cr-original-review-content]").html()
-                    val reviewAuthor: String =
-                        c.select("span[class=a-profile-name]").html()
-                    var reviewDate: String =
-                        c.select("span[data-hook=review-date]").html()
-                    val dateRegExpr="^[0-9][0-9]\\s(January|February|March|April|May|Jun|July|August|September|October|November|December)\\s\\d{4}\$"
-                    val pattern=Pattern.compile(dateRegExpr)
-                    val matcher=pattern.matcher(reviewDate)
-                    val formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy")
-                    //val localReviewDate = LocalDate.parse( matcher.results().findFirst().orElse(null).toString(), formatter)
-                   // if (localReviewDate.isAfter(startDate)) { TODO develop this logic later
-                       // println("new review received")
-                        reviews.add(
-                            Review(
-                                productId = "1",
-                                customerProfileId = c.select("div[id^=customer_review]").attr("id").substring("customer_review-".length),
-                                title = reviewTitle,
-                                author = reviewAuthor,
-                                reviewText = reviewBody,
-                                reviewDate = LocalDate.now()
-                            )
-                        )
-                    }
-               // }
-                //pageNo++
-               // println("page = $pageNo")
-            //} while (allReviewsInPage.size !== 0 &&
-           // )
+                )
+            }
         }
         return reviews
     }
